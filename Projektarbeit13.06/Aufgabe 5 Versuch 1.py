89ba53ee216efc7b46e9d8bfd31d341c8061273a
HermannGermann <105786207+HermannGermann@users.noreply.github.com>
2022-06-21 11:07:57 +0200

    Add files via upload

---
+++ Projektarbeit13.06/Aufgabe 5 Versuch 1.py
@@ -0,0 +1,21 @@
+""""Köhler, Franzi                          xxxx
+Fluch, Moritz                          xxxx
+Müller, Thorben                     348238"""
+
+Raumbuch
+
+1- Raum(Raumbezeichnung:(), Raumnummer:())
+       import Raumvolumen: ()m³
+        Raumumfang: ()m
+        
+        Raumseitenflaeche Brutto: ()m²
+        Raumseitenflaeche Netto: ()m²
+
+Türliste:
+    
+1- Tür (Höhe= ()m, Breite= ()m, Rauchschutz: (), Selbstschließend:(), Notausgang: (), Hersteller: ())
+
+Fensterliste:
+    
+1- Fenster (Höhe: ()m, Breite: ()m, Schallschutzklasse: (), U-Wert= (), Hersteller: (Müller))
+
